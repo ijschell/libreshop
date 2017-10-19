0b9a1679a33df5770e3d8088229041a2c644b32d
Jonathan <jonathans@takeoffmedia.com>
2017-10-19 00:43:58 -0300

    ..

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 function scrollIndicator() {
     $('html, body').animate({
-        scrollTop: $("section").offset().top
+        scrollTop: $("section.top").offset().top
     }, 1000);
 }
 
 $(document).on('mouseover', 'header .wrapper .toolbar a', function(){
   $(this).addClass('bounce');
+})
+
+$(document).ready(function(){
+  var charTop = $(".chars").offset().top - 200;
+  $(window).scroll(function(){
+    console.log(charTop + ' - ' + $(window).scrollTop());
+    if($(window).scrollTop() > charTop){
+      $('section.chars .cat').addClass('fadeInUp');
+      $('section.chars .car').addClass('fadeInLeft');
+      $('section.chars .cel').addClass('fadeInRight');
+    }
+  })
 })
